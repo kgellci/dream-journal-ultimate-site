d6ff17e3a19e49a7981a646bc6431e464a7cfc2b
Kris Gellci <kgellci@gmail.com>
2025-12-12 23:52:58 -0800

    Fix build config to include legal pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './', // Relative base for GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve('index.html'),
+        privacy_policy: resolve('privacy_policy.html'),
+        terms_of_service: resolve('terms_of_service.html'),
+      },
+    },
+  },
 })
