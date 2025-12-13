4d30518150b0522ffecbe0ce9e92239b6c6c1e2d
Kris Gellci <kgellci@gmail.com>
2025-12-12 23:50:29 -0800

    update site

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+console.log('Dream Journal Ultimate loaded.');
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
